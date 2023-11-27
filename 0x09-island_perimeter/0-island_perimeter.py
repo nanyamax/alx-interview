aeabdbd8f8d67d443f539298b7f1d8bb0eed46ca
nanyamax <ifunanyasunday7@gmail.com>
2023-11-27 23:40:10 +0200

    perimenter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,15 @@
+def island_perimeter(grid):
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:  # Land cell
+                perimeter += 4  # Assume all sides are part of the perimeter
+
+                # Check adjacent cells and subtract if they are also land cells
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract two sides for adjacent land cell to the north
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract two sides for adjacent land cell to the west
+
+    return perimeter
